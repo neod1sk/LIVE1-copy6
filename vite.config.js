c663adec02c341d11b784cf23e8e2562e042962a
kversedao <12345678+kversedao@users.noreply.github.com>
2025-11-21 14:51:50 +0900

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 
+const basePath = process.env.NODE_ENV === "production" ? "/LIVE1-copy6/" : "/";
+
 export default defineConfig({
   root: ".",
+  base: basePath,
   server: {
     open: true,
     host: "0.0.0.0",
